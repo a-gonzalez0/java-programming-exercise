75588b6ca334dbb5c1711c80893359da838717a8
Alfredo Gonzalez <a_gonzalez0@yahoo.com>
2021-02-17 20:56:43 -0500

    Update Question11.java

--- chapter4/Question11.java
+++ chapter4/Question11.java
@@ -17,21 +17,21 @@ import java.util.Scanner;
  */
 
 public class Question11 {
-	public static void main(String[] args) {
-		// Create a Scanner object
-		Scanner input = new Scanner(System.in);
+    public static void main(String[] args) {
+        // Create a Scanner object
+        Scanner input = new Scanner(System.in);
 		
-		// Prompt user to enter an integer between 0 and 15
-		System.out.print("Enter a decimal value (0 to 15): ");
-		int decimal = input.nextInt();
+        // Prompt user to enter an integer between 0 and 15
+        System.out.print("Enter a decimal value (0 to 15): ");
+        int decimal = input.nextInt();
 		
-		// Check if correct input and display result
-		if (decimal <= 15 && decimal >= 10) {
-			System.out.print("The hex value is " + (char)(decimal + 'A' - 10));
-		} else if (decimal < 10 && decimal >= 0) {
-			System.out.print("The hex value is " + decimal);
-		} else {
-			System.out.print(decimal + " is an invalid input");
-		}
-	}
+        // Check if correct input and display result
+        if (decimal <= 15 && decimal >= 10) {
+            System.out.print("The hex value is " + (char)(decimal + 'A' - 10));
+        } else if (decimal < 10 && decimal >= 0) {
+            System.out.print("The hex value is " + decimal);
+        } else {
+            System.out.print(decimal + " is an invalid input");
+        }
+    }
 }
