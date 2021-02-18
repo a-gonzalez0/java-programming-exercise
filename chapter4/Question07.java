268a3f2dea6aa7a9f9ccccc99ebfbf143bef089c
Alfredo Gonzalez <a_gonzalez0@yahoo.com>
2021-02-17 20:09:55 -0500

    Update Question07.java

--- chapter4/Question07.java
+++ chapter4/Question07.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 /*
  * (Corner point coordinates) Suppose a pentagon is centered at (0, 0) with one point
- * at the 0 o’clock position, as shown in Figure 4.4c. Write a program that prompts
+ * at the 0 oâ€™clock position, as shown in Figure 4.4c. Write a program that prompts
  * the user to enter the radius of the bounding circle of a pentagon and displays the
  * coordinates of the five corner points on the pentagon from p1 to p5 in this order. Use
  * console format to display two digits after the decimal point. Here is a sample run:
@@ -18,32 +18,32 @@ import java.util.Scanner;
  */
 
 public class Question07 {
-	public static void main(String[] args) {
-		// Create a Scanner object
-		Scanner input = new Scanner(System.in);
+    public static void main(String[] args) {
+        // Create a Scanner object
+        Scanner input = new Scanner(System.in);
 		
-		// Prompt the user to enter the radius
-		System.out.print("Enter the radius of the bounding circle: ");
-		double radius = input.nextDouble();
+        // Prompt the user to enter the radius
+        System.out.print("Enter the radius of the bounding circle: ");
+        double radius = input.nextDouble();
 		
-		// Generate 5 coordinates based on the radius
-		double p1x = radius * Math.cos(Math.PI / 10);
-		double p1y = radius * Math.sin(Math.PI / 10);
-		double p2x = radius * Math.cos(Math.PI / 2);
-		double p2y = radius * Math.sin(Math.PI / 2);
-		double p3x = radius * Math.cos((Math.PI / 2) + (2 * Math.PI / 5));
-		double p3y = radius * Math.sin((Math.PI / 2) + (2 * Math.PI / 5));
-		double p4x = radius * Math.cos((Math.PI / 2) + (4 * Math.PI / 5));
-		double p4y = radius * Math.sin((Math.PI / 2) + (4 * Math.PI / 5));
-		double p5x = radius * Math.cos((Math.PI / 2) + (6 * Math.PI / 5));
-		double p5y = radius * Math.sin((Math.PI / 2) + (6 * Math.PI / 5));
+        // Generate 5 coordinates based on the radius
+        double p1x = radius * Math.cos(Math.PI / 10);
+        double p1y = radius * Math.sin(Math.PI / 10);
+        double p2x = radius * Math.cos(Math.PI / 2);
+        double p2y = radius * Math.sin(Math.PI / 2);
+        double p3x = radius * Math.cos((Math.PI / 2) + (2 * Math.PI / 5));
+        double p3y = radius * Math.sin((Math.PI / 2) + (2 * Math.PI / 5));
+        double p4x = radius * Math.cos((Math.PI / 2) + (4 * Math.PI / 5));
+        double p4y = radius * Math.sin((Math.PI / 2) + (4 * Math.PI / 5));
+        double p5x = radius * Math.cos((Math.PI / 2) + (6 * Math.PI / 5));
+        double p5y = radius * Math.sin((Math.PI / 2) + (6 * Math.PI / 5));
 		
-		// Display the result
-		System.out.println("The coordinates of five points on the pentagon are");
-		System.out.printf("(%4.2f, %4.2f)\n", p1x, p1y);
-		System.out.printf("(%4.2f, %4.2f)\n", p2x, p2y);
-		System.out.printf("(%4.2f, %4.2f)\n", p3x, p3y);
-		System.out.printf("(%4.2f, %4.2f)\n", p4x, p4y);
-		System.out.printf("(%4.2f, %4.2f)", p5x, p5y);
+        // Display the result
+        System.out.println("The coordinates of five points on the pentagon are");
+        System.out.printf("(%4.2f, %4.2f)\n", p1x, p1y);
+        System.out.printf("(%4.2f, %4.2f)\n", p2x, p2y);
+        System.out.printf("(%4.2f, %4.2f)\n", p3x, p3y);
+        System.out.printf("(%4.2f, %4.2f)\n", p4x, p4y);
+        System.out.printf("(%4.2f, %4.2f)", p5x, p5y);
 	}
 }
