c61b5c71b48bceb44f731a0a3a111dc2d35cd00b
Alfredo Gonzalez <a_gonzalez0@yahoo.com>
2021-02-17 20:12:56 -0500

    Update Question09.java

--- chapter4/Question09.java
+++ chapter4/Question09.java
@@ -10,18 +10,18 @@ import java.util.Scanner;
  */
 
 public class Question09 {
-	public static void main(String[] args) {
-		// Create a Scanner object
-		Scanner input = new Scanner(System.in);
+    public static void main(String[] args) {
+        // Create a Scanner object
+        Scanner input = new Scanner(System.in);
 		
-		// Prompt the user to enter a character
-		System.out.print("Enter a character: ");
-		char ch = input.next().charAt(0);
+        // Prompt the user to enter a character
+        System.out.print("Enter a character: ");
+        char ch = input.next().charAt(0);
 		 
-		// Convert character to integer
-		int unicode = (int) ch;
+        // Convert character to integer
+        int unicode = (int) ch;
 		
-		// Display the result
-		System.out.print("The Unicode for the character " +  ch + " is " + unicode);
+        // Display the result
+        System.out.print("The Unicode for the character " +  ch + " is " + unicode);
 	}
 }
