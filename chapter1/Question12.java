f378531dcf60b5905c1a794d91ab94b9975f7dbe
Alfredo Gonzalez <a_gonzalez0@yahoo.com>
2021-02-17 19:20:53 -0500

    Update and rename Question12.java to chapter1/Question12.java

--- chapter1/Question12.java
+++ chapter1/Question12.java
@@ -8,6 +8,7 @@ package chapter1;
 
 public class Question12 {
 	public static void main(String[] args) {
-		System.out.println("The average speed in kilometers is: " + (24.0 * 1.60934) / ((60.0 + 40.0 + (35.0 / 60.0)) / 60.0) + " kph");
+		System.out.println("The average speed in kilometers is: " + 
+		    (24.0 * 1.60934) / ((60.0 + 40.0 + (35.0 / 60.0)) / 60.0) + " kph");
 	}
 }
