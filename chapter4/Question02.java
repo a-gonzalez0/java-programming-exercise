bc4d0ed532b54a62484ae5dee09a0893803798e4
Alfredo Gonzalez <a_gonzalez0@yahoo.com>
2021-02-17 20:05:24 -0500

    Update Question02.java

--- chapter4/Question02.java
+++ chapter4/Question02.java
@@ -22,25 +22,25 @@ import java.util.Scanner;
  */
 
 public class Question02 {
-	public static void main(String[] args) {
-		// Create a Scanner object
-		Scanner input = new Scanner(System.in);
+    public static void main(String[] args) {
+        // Create a Scanner object
+        Scanner input = new Scanner(System.in);
 		
-		// Prompt the user to enter the latitude and longitude
-		System.out.print("Enter point 1 (latitude and longitude) in degrees: ");
-		double x1 = Math.toRadians(input.nextDouble());
-		double y1 = Math.toRadians(input.nextDouble());
+        // Prompt the user to enter the latitude and longitude
+        System.out.print("Enter point 1 (latitude and longitude) in degrees: ");
+        double x1 = Math.toRadians(input.nextDouble());
+        double y1 = Math.toRadians(input.nextDouble());
 		
-		System.out.print("Enter point 2 (latitude and longitude) in degrees: ");
-		double x2 = Math.toRadians(input.nextDouble());
-		double y2 = Math.toRadians(input.nextDouble());
+        System.out.print("Enter point 2 (latitude and longitude) in degrees: ");
+        double x2 = Math.toRadians(input.nextDouble());
+        double y2 = Math.toRadians(input.nextDouble());
 		
-		final double RADIUS = 6371.01;
+        final double RADIUS = 6371.01;
 		
-		double distance = RADIUS * Math.acos(Math.sin(x1) * Math.sin(x2) +
-				Math.cos(x1) * Math.cos(x2) * Math.cos(y1 - y2));
+        double distance = RADIUS * Math.acos(Math.sin(x1) * Math.sin(x2) +
+            Math.cos(x1) * Math.cos(x2) * Math.cos(y1 - y2));
 		
-		// Display the result
-		System.out.print("The distance between the two points is " + distance + " km");
+        // Display the result
+        System.out.print("The distance between the two points is " + distance + " km");
 	}
 }
